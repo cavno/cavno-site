7dbae0dc0ecfb64ad422e13f82f1d8e64b621442
tchow <chowmengen@gmail.com>
2026-07-30 01:58:57 +0800

    sum

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // 部署后改成实际地址：https://cavno.org 或自定义域名
-  site: 'https://cavno.org',
+  // 部署后改成实际地址：https://cavno.pages.dev 或自定义域名
+  site: 'https://cavno.pages.dev',
   trailingSlash: 'ignore',
 });
